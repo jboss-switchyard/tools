b1accedc74b609fb6b78b701d56dad749414ccc8
rcernich <rcernich@redhat.com>
2012-03-05 09:46:34 -0700

    SWITCHYARD-668 change licensing from LGPL to EPL

--- eclipse/tests/org.switchyard.tools.m2e.tests/src/org/switchyard/tools/m2e/tests/SwitchYardConfigurationTest.java
+++ eclipse/tests/org.switchyard.tools.m2e.tests/src/org/switchyard/tools/m2e/tests/SwitchYardConfigurationTest.java
@@ -1,21 +1,13 @@
-/* 
- * JBoss, Home of Professional Open Source 
- * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
- * as indicated by the @author tags. All rights reserved. 
- * See the copyright.txt in the distribution for a 
- * full listing of individual contributors.
+/*************************************************************************************
+ * Copyright (c) 2011 Red Hat, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * This copyrighted material is made available to anyone wishing to use, 
- * modify, copy, or redistribute it subject to the terms and conditions 
- * of the GNU Lesser General Public License, v. 2.1. 
- * This program is distributed in the hope that it will be useful, but WITHOUT A 
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
- * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details. 
- * You should have received a copy of the GNU Lesser General Public License, 
- * v.2.1 along with this distribution; if not, write to the Free Software 
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
- * MA  02110-1301, USA.
- */
+ * Contributors:
+ *     JBoss by Red Hat - Initial implementation.
+ ************************************************************************************/
 package org.switchyard.tools.m2e.tests;
 
 import java.io.InputStreamReader;
