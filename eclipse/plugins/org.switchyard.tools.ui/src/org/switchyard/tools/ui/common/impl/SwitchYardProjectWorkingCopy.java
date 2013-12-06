167c88e7dd3e9108ba2ea45075dda00fea8c9d4a
rcernich <rcernich@redhat.com>
2013-12-06 14:54:05 -0700

    SWITCHYARD-1655 preserve pom.xml structure when updating sy components

--- eclipse/plugins/org.switchyard.tools.ui/src/org/switchyard/tools/ui/common/impl/SwitchYardProjectWorkingCopy.java
+++ eclipse/plugins/org.switchyard.tools.ui/src/org/switchyard/tools/ui/common/impl/SwitchYardProjectWorkingCopy.java
@@ -10,13 +10,13 @@
  ************************************************************************************/
 package org.switchyard.tools.ui.common.impl;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -29,14 +29,25 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubProgressMonitor;
-import org.eclipse.m2e.core.MavenPlugin;
-import org.eclipse.m2e.core.internal.IMavenConstants;
+import org.eclipse.jface.text.DocumentRewriteSession;
+import org.eclipse.jface.text.DocumentRewriteSessionType;
+import org.eclipse.jface.text.IDocumentExtension4;
+import org.eclipse.m2e.core.ui.internal.editing.AddDependencyOperation;
+import org.eclipse.m2e.core.ui.internal.editing.PomEdits;
+import org.eclipse.m2e.core.ui.internal.editing.PomHelper;
+import org.eclipse.m2e.core.ui.internal.editing.RemoveDependencyOperation;
+import org.eclipse.wst.sse.core.StructuredModelManager;
+import org.eclipse.wst.sse.core.internal.undo.IStructuredTextUndoManager;
+import org.eclipse.wst.xml.core.internal.provisional.document.IDOMDocument;
+import org.eclipse.wst.xml.core.internal.provisional.document.IDOMModel;
 import org.switchyard.config.model.switchyard.SwitchYardModel;
 import org.switchyard.tools.ui.Activator;
+import org.switchyard.tools.ui.M2EUtils;
 import org.switchyard.tools.ui.common.ISwitchYardComponentExtension;
 import org.switchyard.tools.ui.common.ISwitchYardProjectWorkingCopy;
 import org.switchyard.tools.ui.common.SwitchYardComponentExtensionManager;
 import org.switchyard.tools.ui.i18n.Messages;
+import org.w3c.dom.Element;
 
 /**
  * SwitchYardProjectWorkingCopy
@@ -251,16 +262,17 @@ public class SwitchYardProjectWorkingCopy implements ISwitchYardProjectWorkingCo
             monitor.beginTask(Messages.SwitchYardProjectWorkingCopy_taskMessage_updatingProjectPOM, 300);
             IProgressMonitor subMonitor = new SubProgressMonitor(monitor, 100,
                     SubProgressMonitor.PREPEND_MAIN_LABEL_TO_SUBTASK);
-            if (processUpdates(subMonitor)) {
+            final List<Operation> updateOperations = processUpdates(subMonitor);
+            if (updateOperations.size() > 0) {
                 subMonitor.done();
 
-                // write out the changes
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                MavenPlugin.getMaven().writeModel(getMavenProject().getOriginalModel(), baos);
-
                 subMonitor = new SubProgressMonitor(monitor, 100, SubProgressMonitor.PREPEND_MAIN_LABEL_TO_SUBTASK);
-                getProject().getFile(IMavenConstants.POM_FILE_NAME).setContents(
-                        new ByteArrayInputStream(baos.toByteArray()), true, true, subMonitor);
+                try {
+                    updatePom(updateOperations);
+                } catch (IOException e) {
+                    throw new CoreException(
+                            new Status(Status.ERROR, Activator.PLUGIN_ID, "Could not update pom.xml", e));
+                }
                 subMonitor.done();
             }
             subMonitor.done();
@@ -289,8 +301,8 @@ public class SwitchYardProjectWorkingCopy implements ISwitchYardProjectWorkingCo
         _switchYardProject.disposed(this);
     }
 
-    private boolean processUpdates(IProgressMonitor monitor) throws CoreException {
-        boolean modelUpdated = false;
+    private List<Operation> processUpdates(IProgressMonitor monitor) throws CoreException {
+        final List<Operation> operations = new ArrayList<Operation>();
 
         monitor.beginTask(Messages.SwitchYardProjectWorkingCopy_taskMessage_processingSYSettingsChanges, 50);
 
@@ -307,11 +319,9 @@ public class SwitchYardProjectWorkingCopy implements ISwitchYardProjectWorkingCo
         final String currentSwitchYardVersion = _switchYardProject.getVersion();
         if (switchYardVersionPropertyKey != null) {
             if (_newVersion != null) {
-                model.addProperty(switchYardVersionPropertyKey, _newVersion);
-                modelUpdated = true;
+                operations.add(new UpdatePropertyOperation(switchYardVersionPropertyKey, _newVersion));
             } else if (currentSwitchYardVersion == null) {
-                model.addProperty(switchYardVersionPropertyKey, ""); //$NON-NLS-1$
-                modelUpdated = true;
+                operations.add(new UpdatePropertyOperation(switchYardVersionPropertyKey, "")); //$NON-NLS-1$
             }
         }
         monitor.worked(10);
@@ -320,16 +330,20 @@ public class SwitchYardProjectWorkingCopy implements ISwitchYardProjectWorkingCo
 
         if (_removedComponents.size() > 0) {
             // there's got to be a better way....
+            final List<Dependency> removedDependencies = new ArrayList<Dependency>();
             for (ISwitchYardComponentExtension component : _removedComponents.values()) {
                 for (Dependency dependency : component.getDependencies()) {
                     for (Iterator<Dependency> it = model.getDependencies().iterator(); it.hasNext();) {
-                        if (it.next().getManagementKey().equals(dependency.getManagementKey())) {
-                            it.remove();
-                            modelUpdated = true;
+                        final Dependency existingDependency = it.next();
+                        if (existingDependency.getManagementKey().equals(dependency.getManagementKey())) {
+                            removedDependencies.add(existingDependency);
                             break;
                         }
                     }
                 }
+            }
+            if (removedDependencies.size() > 0) {
+                operations.add(new RemoveDependenciesOperation(removedDependencies));
             }
         }
         monitor.worked(10);
@@ -338,6 +352,7 @@ public class SwitchYardProjectWorkingCopy implements ISwitchYardProjectWorkingCo
         addComponent(SwitchYardComponentExtensionManager.instance().getRuntimeComponentExtension());
         if (_addedComponents.size() > 0) {
             final String versionString = getRawVersionString();
+            final List<Dependency> addedDependencies = new ArrayList<Dependency>();
             for (ISwitchYardComponentExtension component : _addedComponents.values()) {
                 COMPONENT_DEPENDENCIES: for (Dependency dependency : component.getDependencies()) {
                     // crude, but effective
@@ -349,17 +364,24 @@ public class SwitchYardProjectWorkingCopy implements ISwitchYardProjectWorkingCo
                     }
                     dependency = dependency.clone();
                     dependency.setVersion(versionString);
-                    model.getDependencies().add(dependency);
-                    modelUpdated = true;
+                    addedDependencies.add(dependency);
                 }
+            }
+            if (addedDependencies.size() > 0) {
+                operations.add(new AddDependenciesOperation(addedDependencies));
             }
         }
         monitor.worked(10);
 
         monitor.subTask(Messages.SwitchYardProjectWorkingCopy_taskMessage_validatingSYPlugin);
-        if (_switchYardProject.getPlugin().updateScannerClasses(collectScanners(_addedComponents.values()),
-                collectScanners(_removedComponents.values()))) {
-            modelUpdated = true;
+        final Set<String> existingScanners = _switchYardProject.getPlugin().getScannerClasses();
+        final Set<String> addedScanners = collectScanners(_addedComponents.values());
+        final Set<String> removedScanners = collectScanners(_removedComponents.values());
+        removedScanners.removeAll(addedScanners);
+        addedScanners.removeAll(existingScanners);
+        removedScanners.retainAll(existingScanners);
+        if (addedScanners.size() > 0 || removedScanners.size() > 0) {
+            operations.add(new UpdateScannersOperation(addedScanners, removedScanners));
         }
         monitor.worked(10);
 
@@ -389,7 +411,7 @@ public class SwitchYardProjectWorkingCopy implements ISwitchYardProjectWorkingCo
 
         monitor.done();
 
-        return modelUpdated;
+        return operations;
     }
 
     private Set<String> collectScanners(Collection<ISwitchYardComponentExtension> components) {
@@ -402,4 +424,207 @@ public class SwitchYardProjectWorkingCopy implements ISwitchYardProjectWorkingCo
         }
         return scanners;
     }
+
+    /**
+     * performs an modifying operation on top the
+     * 
+     * Adapted from org.eclipse.m2e.core.ui.internal.editing.PomEdits.performOnDOMDocument()
+     */
+    private void updatePom(List<Operation> operations) throws IOException, CoreException {
+        final IDOMModel domModel = (IDOMModel) StructuredModelManager.getModelManager().getModelForEdit(
+                _switchYardProject.getMavenProjectFacade().getPom());
+        if (domModel == null) {
+            throw new CoreException(new Status(Status.ERROR, Activator.PLUGIN_ID, "Could not update pom.xml."));
+        }
+        
+        try {
+            DocumentRewriteSession session = null;
+            IStructuredTextUndoManager undo = null;
+
+            // let the model know we make changes
+            domModel.aboutToChangeModel();
+            undo = domModel.getStructuredDocument().getUndoManager();
+            // let the document know we make changes
+            if (domModel.getStructuredDocument() instanceof IDocumentExtension4) {
+                IDocumentExtension4 ext4 = (IDocumentExtension4) domModel.getStructuredDocument();
+                session = ext4.startRewriteSession(DocumentRewriteSessionType.UNRESTRICTED_SMALL);
+            }
+            undo.beginRecording(domModel);
+
+            try {
+                for (Operation operation : operations) {
+                    try {
+                        operation.process(domModel.getDocument());
+                    } catch (Exception e) {
+                        // XXX: should we log an error?
+                        e.printStackTrace();
+                    }
+                }
+            } finally {
+                undo.endRecording(domModel);
+                if (session != null && domModel.getStructuredDocument() instanceof IDocumentExtension4) {
+                    IDocumentExtension4 ext4 = (IDocumentExtension4) domModel.getStructuredDocument();
+                    ext4.stopRewriteSession(session);
+                }
+                domModel.changedModel();
+            }
+        } finally {
+            try {
+                domModel.save();
+            } finally {
+                domModel.releaseFromEdit();
+            }
+        }
+    }
+
+    private static interface Operation {
+        public void process(IDOMDocument document);
+    }
+
+    private final class UpdatePropertyOperation implements Operation {
+
+        private final String _key;
+        private final String _value;
+
+        private UpdatePropertyOperation(String key, String value) {
+            _key = key;
+            _value = value;
+        }
+
+        @Override
+        public void process(IDOMDocument document) {
+            final Element property = PomEdits.getChild(document.getDocumentElement(), PomEdits.PROPERTIES, _key);
+            PomEdits.setText(property, _value);
+        }
+    }
+
+    private final class RemoveDependenciesOperation implements Operation {
+
+        private final List<Dependency> _removedDependencies;
+
+        private RemoveDependenciesOperation(List<Dependency> removedDependencies) {
+            _removedDependencies = removedDependencies;
+        }
+
+        @Override
+        public void process(IDOMDocument document) {
+            for (Dependency dependency : _removedDependencies) {
+                try {
+                    new RemoveDependencyOperation(dependency).process(document);
+                } catch (Exception e) {
+                    // TODO: log an error?
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private final class AddDependenciesOperation implements Operation {
+
+        private final List<Dependency> _addedDependencies;
+
+        private AddDependenciesOperation(List<Dependency> addedDependencies) {
+            _addedDependencies = addedDependencies;
+        }
+
+        @Override
+        public void process(IDOMDocument document) {
+            for (Dependency dependency : _addedDependencies) {
+                try {
+                    new AddDependencyOperation(dependency).process(document);
+                } catch (Exception e) {
+                    // TODO: log an error?
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private final class UpdateScannersOperation implements Operation {
+
+        private final Set<String> _addedScanners;
+        private final Set<String> _removedScanners;
+
+        private UpdateScannersOperation(Set<String> addedScanners, Set<String> removedScanners) {
+            _addedScanners = addedScanners;
+            _removedScanners = removedScanners;
+        }
+
+        @Override
+        public void process(IDOMDocument document) {
+            Element switchYardPlugin = PomEdits.findChild(PomEdits.findChild(
+                    PomEdits.findChild(document.getDocumentElement(), PomEdits.BUILD), PomEdits.PLUGINS),
+                    PomEdits.PLUGIN, PomEdits.childEquals(PomEdits.GROUP_ID, M2EUtils.SWITCHYARD_CORE_GROUP_ID),
+                    PomEdits.childEquals(PomEdits.ARTIFACT_ID, M2EUtils.SWITCHYARD_PLUGIN_ARTIFACT_ID));
+            if (switchYardPlugin == null) {
+                if (_addedScanners.size() == 0 || _switchYardProject.getPlugin().getPlugin() != null) {
+                    /*
+                     * if we're not adding anything or the plugin is declared in
+                     * a parent pom, just bail.
+                     */
+                    return;
+                }
+                Element pluginsElement = PomEdits.getChild(document.getDocumentElement(), PomEdits.BUILD, PomEdits.PLUGINS);
+                switchYardPlugin = PomHelper.createPlugin(pluginsElement, M2EUtils.SWITCHYARD_CORE_GROUP_ID, M2EUtils.SWITCHYARD_PLUGIN_ARTIFACT_ID, getRawVersionString());
+                Element goalElement = PomEdits.getChild(switchYardPlugin, PomEdits.EXECUTIONS, PomEdits.EXECUTION, PomEdits.GOALS, PomEdits.GOAL);
+                PomEdits.setText(goalElement, M2EUtils.CONFIGURE_GOAL);
+                PomEdits.getChild(switchYardPlugin, PomEdits.CONFIGURATION);
+            }
+
+            Element configElement = PomEdits.findChild(switchYardPlugin, PomEdits.CONFIGURATION);
+            if (configElement == null) {
+                List<Element> executionElements = PomEdits.findChilds(
+                        PomEdits.findChild(switchYardPlugin, PomEdits.EXECUTIONS), PomEdits.EXECUTION);
+                boolean addConfig = _addedScanners.size() > 0;
+                if (executionElements.size() > 0) {
+                    for (Element executionElement : executionElements) {
+                        if (PomEdits.findChild(PomEdits.findChild(executionElement, PomEdits.GOALS), PomEdits.GOAL,
+                                PomEdits.textEquals(M2EUtils.CONFIGURE_GOAL)) == null) {
+                            continue;
+                        }
+                        Element executionConfigElement = PomEdits.findChild(executionElement, PomEdits.CONFIGURATION);
+                        if (executionConfigElement != null) {
+                            updateScanners(executionConfigElement);
+                            addConfig = false;
+                            /*
+                             * we'll only process the first execution. not sure
+                             * how to handle multiple executions. i'm assuming
+                             * we wouldn't want them to all have the same
+                             * configuration.
+                             */
+                            break;
+                        }
+                    }
+                }
+                if (addConfig) {
+                    configElement = PomEdits.createElement(switchYardPlugin, PomEdits.CONFIGURATION);
+                    updateScanners(configElement);
+                }
+            } else {
+                updateScanners(configElement);
+            }
+        }
+        
+        private void updateScanners(Element configElement) {
+            Element scannersElement = PomEdits.findChild(configElement, M2EUtils.SCANNER_CLASS_NAMES_ELEMENT);
+            if (scannersElement == null) {
+                if (_addedScanners.size() == 0) {
+                    return;
+                }
+                scannersElement = PomEdits.getChild(configElement, M2EUtils.SCANNER_CLASS_NAMES_ELEMENT);
+            }
+            for (String scanner : _removedScanners) {
+                PomEdits.removeChild(scannersElement, PomEdits.findChild(scannersElement, M2EUtils.PARAM_ELEMENT, PomEdits.textEquals(scanner)));
+            }
+            for (String scanner : _addedScanners) {
+                Element paramElement = PomEdits.findChild(scannersElement, M2EUtils.PARAM_ELEMENT, PomEdits.textEquals(scanner));
+                if (paramElement == null) {
+                    paramElement = PomEdits.createElementWithText(scannersElement, M2EUtils.PARAM_ELEMENT, scanner);
+                    PomEdits.format(paramElement);
+                }
+            }
+            PomEdits.removeIfNoChildElement(scannersElement);
+        }
+    }
+
 }
